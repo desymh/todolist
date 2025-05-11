bb72022c8daee659499dc8ac4ef4a4d0db58f699
desy <desymamluahhimaya@gmail.com>
2025-05-11 09:49:56 +0700

    hapus widget

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.todolist"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // ← Ini ditambahkan secara eksplisit
+    ndkVersion = "26.3.11579264" // ← Ini ditambahkan secara eksplisit
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
